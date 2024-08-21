7387d5e0c92ccba9b2901c055faff2c576487453
fpollockhc <fraser@hashicorp.com>
2024-08-21 10:57:13 -0400

    Push

--- main.tf
+++ main.tf
@@ -1,18 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web" {
+source "aws_instance" "web" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t3.micro"
 
