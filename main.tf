c46f880e215f363c206f129f05b428b43fa54e62
fpollockhc <fraser@hashicorp.com>
2024-08-21 11:01:07 -0400

    Fix dumb mistake

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-source "aws_instance" "web" {
+resource "aws_instance" "web" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t3.micro"
 
