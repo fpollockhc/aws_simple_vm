0b1e565266a2d296ffcfed3cdae64ed95e9649c0
Fraser Pollock <fraser@hashicorp.com>
2025-03-10 10:06:28 -0400

    Hardcoded

--- main.tf
+++ main.tf
@@ -3,20 +3,10 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_subnet" "example" {
-  vpc_id            = "vpc-0003dc24dd09db43f"
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-2a"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t3.micro"
-  subnet_id = aws_subnet.example.id
+  subnet_id = "subnet-02287f9116fc45b62"
 
   tags = {
     Name = "HelloWorld"
