b8afcb6fa8e6ce25cdaac6aebcfd2928fc404bc4
Fraser Pollock <fraser@hashicorp.com>
2025-03-10 09:37:38 -0400

    S3 faster

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0cff7528ff583bf9a"
-  instance_type = "t3.micro"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
 
   tags = {
-    Name = "HelloWorld"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
