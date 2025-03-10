e6162766c53dc83fcc63fb18e3eb99eb34ff138e
Fraser Pollock <fraser@hashicorp.com>
2025-03-10 09:59:10 -0400

    Network comp

--- main.tf
+++ main.tf
@@ -3,6 +3,24 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t3.micro"
